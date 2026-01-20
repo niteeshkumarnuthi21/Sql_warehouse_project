d06a2ac9d56fcbad6e7c04c6bc62f8c0a0464df9
niteeshkumarnuthi21 <niteeshkumarnuthi21@gmail.com>
2026-01-19 19:55:52 -0600

    Update setup.sql

--- scripts/setup.sql
+++ scripts/setup.sql
@@ -1,1 +1,24 @@
 
+--Creating database and schemas--
+--The script creates a new database name "Datawarehouse" 
+-- ON top of that create schemas within the database 'bronze', 'sliver', 'gold'
+
+
+
+USE mster;
+GO
+
+--Create the Datawarehouse Database--
+CREATE DATABASE Datawarehouse;
+GO
+
+USE Datawarehouse;
+GO
+
+-- Create Schemas in Database
+CREATE SCHEMA bronze;
+GO
+CREATE SCHEMA sliver;
+GO
+CREATE SCHEMA gold;
+GO
